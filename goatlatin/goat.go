b0c5dd758e57e3b6d029f61bc32e2a40a5a68952
Timour Ezeev <timour.ezeev@me.com>
2018-10-04 18:52:52 -0400

    Make sure the output is pure ascii

--- goatlatin/goat.go
+++ goatlatin/goat.go
@@ -24,66 +24,76 @@ should print: 'Imaa peaksmaaa oatGmaaaa atinLmaaaaa'
 package main
 
 import (
-        "fmt"
-        "regexp"
-        "strings"
+	"fmt"
+	"regexp"
+	"strings"
 )
 
 // Global splitter
 var splitter = regexp.MustCompile("[ \t\v]")
 
 func main() {
-        string_to_goat_latin("I speak Goat Latin")
+	string_to_goat_latin("I speak Goat Latin")
 }
 
 func string_to_goat_latin(s string) {
-        words := splitter.Split(s, -1)
-        trans := make([]string, 0, len(words))
-        // Translate word by word
-        for i, word := range words {
-                trans = append(trans, translateWord(word, i+1))
-        }
-        // Print translation
-        fmt.Println(strings.Join(trans, " "))
+	words := splitter.Split(s, -1)
+	trans := make([]string, 0, len(words))
+	// Translate word by word
+	for i, word := range words {
+		trans = append(trans, translateWord(word, i+1))
+	}
+	// Print translation
+	fmt.Println(strings.Join(trans, " "))
 }
 
 // Translate word
 func translateWord(word string, n int) string {
-        // Split word into slice of runes
-        runes := []rune(word)
-        // Pre-allocate maximum needed buffer on the stack
-        buf := make([]rune, len(runes)+1+2+n)
-        // Copy word's runes into our local buffer
-        copy(buf, runes)
-        // Check if the first letter is a consonant
-        if !isVowel(buf[0]) {
-                // Rule #1
-                buf = append(buf[1:], buf[0])
-        }
-        // Rule #2 + end of rule #1
-        buf = append(buf, 'm', 'a')
-        // Rule #3
-        buf = append(buf, repeatRune('a', n)...)
-        // Return translated word as a string
-        return string(buf)
+	// Split word into slice of runes
+	runes := []rune(word)
+	// Pre-allocate maximum needed buffer on the stack
+	buf := make([]rune, len(runes)+1+2+n)
+	// Copy word's runes into our local buffer
+	copy(buf, runes)
+	// Check if the first letter is a consonant
+	if !isVowel(buf[0]) {
+		// Rule #1
+		buf = append(buf[1:], buf[0])
+	}
+	// Rule #2 + end of rule #1
+	buf = append(buf, 'm', 'a')
+	// Rule #3
+	buf = append(buf, repeatRune('a', n)...)
+	// Return translated word as a string
+	return string(squize(buf))
 }
 
 // Check if rune is an English vowel
 func isVowel(r rune) bool {
-        switch r {
-        // a e i o u A E I O U
-        case 'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U':
-                return true
-        default:
-                return false
-        }
+	switch r {
+	// a e i o u A E I O U
+	case 'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U':
+		return true
+	default:
+		return false
+	}
 }
 
 // Generate slice of N runes
 func repeatRune(r rune, n int) []rune {
-        res := make([]rune, n)
-        for i := range res {
-                res[i] = r
-        }
-        return res
+	res := make([]rune, n)
+	for i := range res {
+		res[i] = r
+	}
+	return res
+}
+
+func squize(runes []rune) []byte {
+	res := make([]byte, 0, len(runes))
+	for _, v := range []byte(string(runes)) {
+		if v > 0 {
+			res = append(res, v)
+		}
+	}
+	return res
 }
